87814008dab222014227f6953d5e06997f00939c
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-25 21:55:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,9 @@ class InteractiveRecord
   end 
   
   def self.find_by(attribute) 
-    
+    binding.pry 
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end 
   
 
