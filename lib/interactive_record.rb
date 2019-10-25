d1252a5a94f9fadabbe5c5ce7363c24e604ca484
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-25 21:17:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,6 +3,10 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym 
+  end 
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end 
