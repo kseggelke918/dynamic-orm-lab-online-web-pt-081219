9df41dd4c9920257448d96a5404c4691efa61789
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-25 21:52:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,11 +19,6 @@ class InteractiveRecord
     end 
     column_names.compact 
   end 
-  
-  self.column_names.each do |col_name|
-    binding.pry 
-    attr_accessor col_name.to_sym 
-  end 
 
   def initialize(options={})
     options.each do |property, value|
